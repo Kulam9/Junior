9efae89734929e51ad427e3304faf8f0d48c9ed2
Kulam <kulam.521587589gmail.com@MacBook-Air-Kulam.local>
2024-02-08 22:29:02 +0300

    add displey rule

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1 className='kek'>haio</h1>hai
         </p>
         <a
           className="App-link"
